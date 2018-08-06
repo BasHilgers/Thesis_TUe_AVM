42e0c3816ca01b4ed8f86cb58fa858ab1562045e
BasHilgers <34332095+BasHilgers@users.noreply.github.com>
2018-08-07 00:03:09 +0200

    Add files via upload

---
+++ 0_software_01082018.R
@@ -0,0 +1,32 @@
+##########################################################################################
+#                                                                                        #            
+#  Necessary R Packages MasterThesis - Automated Valuation of Commercial Real Estate     #   
+#                                                                                        #                  
+##########################################################################################
+
+## Install the necessary libraries
+
+  install.packages('tidyverse')
+  install.packages('sf')
+  install.packages('sp')
+  install.packages('maptools')
+  install.packages('rgeos')
+  install.packages('spgwr')
+  install.packages('spdep')
+  install.packages('geosphere')
+  install.packages('OpenStreetMap')
+  install.packages('gstat')
+  install.packages('RODBC')
+  install.packages('RSQLite')
+  install.packages('BMA')
+  install.packages('MASS')
+  install.packages('car')
+  install.packages('lmtest')
+  install.packages('plm')
+  install.packages('scales')
+  install.packages('GGally')
+  install.packages('ggplot2')
+  install.packages('scales')
+  
+##########################################################################################
+##########################################################################################
